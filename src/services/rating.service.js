1f64d2f8d0ee9d54ed9b7cd0402361e242db7487
Dustin Nemes <39643802+some-things@users.noreply.github.com>
2022-02-21 21:12:31 -0600

    implement rating movies

--- src/services/rating.service.js
+++ src/services/rating.service.js
@@ -1,4 +1,5 @@
-import { goodfellas } from '../../test/fixtures/movies.js'
+import { int } from 'neo4j-driver'
+
 import { ratings } from '../../test/fixtures/ratings.js'
 import NotFoundError from '../errors/not-found.error.js'
 import { toNativeTypes } from '../utils.js'
@@ -12,11 +13,11 @@ export default class ReviewService {
   driver
 
   /**
-  * The constructor expects an instance of the Neo4j Driver, which will be
-  * used to interact with Neo4j.
-  *
-  * @param {neo4j.Driver} driver
-  */
+   * The constructor expects an instance of the Neo4j Driver, which will be
+   * used to interact with Neo4j.
+   *
+   * @param {neo4j.Driver} driver
+   */
   constructor(driver) {
     this.driver = driver
   }
@@ -38,7 +39,7 @@ export default class ReviewService {
    * @returns {Promise<Record<string, any>>}
    */
   // tag::forMovie[]
-  async forMovie(id, sort = 'timestamp', order = 'ASC', limit = 6, skip = 0) {
+  async forMovie(id, sort = "timestamp", order = "ASC", limit = 6, skip = 0) {
     // TODO: Get ratings for a Movie
 
     return ratings
@@ -60,11 +61,49 @@ export default class ReviewService {
   // tag::add[]
   async add(userId, movieId, rating) {
     // TODO: Convert the native integer into a Neo4j Integer
-    // TODO: Save the rating in the database
-    // TODO: Return movie details and a rating
+    rating = int(rating)
 
-    return goodfellas
+    // TODO: Save the rating in the database
+    // Open a new session
+    const session = this.driver.session()
+
+    // Run the cypher query
+    const res = await session.writeTransaction((tx) =>
+      tx.run(
+        `
+        MATCH (u:User {userId: $userId})
+        MATCH (m:Movie {tmdbId: $movieId})
+
+        MERGE (u)-[r:RATED]->(m)
+        SET r.rating = $rating,
+          r.timestamp = timestamp()
+
+        RETURN m {
+          .*,
+          rating: r.rating
+        } AS movie
+      `,
+        { userId, movieId, rating }
+      )
+    )
+
+    // Close the session
+    await session.close()
+
+    // User or Movie not found
+    if (res.records.length === 0) {
+      throw new NotFoundError(
+        `Could not create rating for Movie ${movieId} by User ${userId}`
+      )
+    }
+
+    // TODO: Return movie details and a rating
+    const [first] = res.records
+    const movie = first.get("movie")
+
+    return toNativeTypes(movie)
+
+    // return goodfellas
   }
   // end::add[]
-
 }
